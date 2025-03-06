90898cabb97701a46b5a96e65accab462a931ef0
Dywinar <dywinards@tutanota.com>
2025-03-06 20:29:52 +0300

    Добавлена валидация данных, улучшена документация

--- src/main/java/com/webapp/springBoot/controllers/PersonController.java
+++ src/main/java/com/webapp/springBoot/controllers/PersonController.java
@@ -1,38 +1,31 @@
 package com.webapp.springBoot.controllers;
 
 
-import com.webapp.springBoot.DTO.APiResponceUserDOTO;
-import com.webapp.springBoot.DTO.ApiREsponceDeleteIDDOTO;
-import com.webapp.springBoot.DTO.ApiResponceDocsDOTO;
-import com.webapp.springBoot.DTO.ApiResponceListUsersDOTO;
+import com.webapp.springBoot.DTO.Person.APiResponceUserDTO;
+import com.webapp.springBoot.DTO.ApiResponceDocsDTO;
+import com.webapp.springBoot.DTO.Person.ApiResponceListUsersDTO;
 import com.webapp.springBoot.entity.Users;
+import com.webapp.springBoot.exception.ValidationErrorWithMethod;
 import com.webapp.springBoot.service.UsersService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Map;
+
 
 @RestController
 @RequestMapping("/api/user")
 public class PersonController {
     @Autowired
     private UsersService usersService;
-    @Operation(
-        summary = "Проверка подключения к Api",
-        responses = {
-                @ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = ApiResponceDocsDOTO.class)))
-        }
-    )
-    @GetMapping("/check")
-    public Map<String, String> checkAPIConnect(){
-        return Map.of("messages", "The API is working correct");
-    }
 
 
 
@@ -40,39 +33,32 @@ public class PersonController {
     @Operation(
             summary="Добавление нового пользователя",
             responses = {
-                    @ApiResponse(responseCode = "201", content = @Content(schema = @Schema(implementation = APiResponceUserDOTO.class)))
-            },
-            parameters = @Parameter(name="users", description = "информация о пользователе", required = true, schema = @Schema(implementation = APiResponceUserDOTO.class))
-    )
-    public Users saveNewUser(@RequestBody Users users){
-        usersService.saveUser(users);
-        return users;
+                    @ApiResponse(responseCode = "201", content = @Content(schema = @Schema(implementation = String.class))),
+                    @ApiResponse(responseCode = "400", content = @Content(schema = @Schema(implementation = String.class)))
+            })
+    public ResponseEntity<String> saveNewUser(@RequestBody @Valid APiResponceUserDTO users, BindingResult result) throws ValidationErrorWithMethod {
+        usersService.saveUser(users, result);
+        return ResponseEntity.ok("Пользователь добавлен");
     }
-
-
-
 
 
     @PostMapping("/findByName")
     @Operation(
             summary="Поиск пользователя по имени",
             responses = {
-                    @ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = ApiResponceListUsersDOTO.class)))
-            },
-            parameters = @Parameter(name="Имя", description = "Поиск по имени пользователя", required = true, schema = @Schema(implementation = ApiResponceDocsDOTO.class))
-    )
+                    @ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = ApiResponceListUsersDTO.class))),
+                    @ApiResponse(responseCode = "400", content = @Content(schema = @Schema(implementation = String.class)))
+
+            })
     public List<Users> findUserByName(@RequestParam String name){
         return usersService.getUserByName(name);
     }
 
-
-
-
-    @PostMapping("/findAll")
+    @GetMapping("/findAll")
     @Operation(
             summary="Вывод всех пользователей",
             responses = {
-                    @ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = ApiResponceListUsersDOTO.class)))
+                    @ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = ApiResponceListUsersDTO.class)))
             }
     )
     public List<Users> findAllUser(){
@@ -84,12 +70,43 @@ public class PersonController {
     @Operation(
             summary="Удаления пользователя по ID",
             responses = {
-                    @ApiResponse(responseCode = "202", content = @Content(schema = @Schema(implementation = ApiResponceDocsDOTO.class)))
+                    @ApiResponse(responseCode = "202", content = @Content(schema = @Schema(implementation = ApiResponceDocsDTO.class))),
+                    @ApiResponse(responseCode = "404", content = @Content(schema = @Schema(implementation = String.class)))
             },
-            parameters = @Parameter(name="id", description = "ID пользователя", required = true, schema = @Schema(implementation = ApiREsponceDeleteIDDOTO.class))
+            parameters = @Parameter(name="id", description = "ID пользователя", required = true)
     )
-    public ApiResponceDocsDOTO deleteUserByID(@RequestParam Long id) throws Exception {
-        String stringResponce = String.format("Пользователь с id=%d удален его имя было: %s", id, usersService.deleteUserByID(id).toString());
-        return new ApiResponceDocsDOTO(stringResponce);
+    public ResponseEntity<String> deleteUserByID(@RequestParam Long id) {
+            usersService.deleteUserByID(id);
+            return ResponseEntity.ok("Пользователь был успешно удален");
     }
-}
+
+    @GetMapping("/ageBetween")
+    @Operation(
+            summary = "Получение пользователе по возрасту в определенном промежутке",
+            responses = @ApiResponse(
+                    responseCode = "200", content = @Content(schema = @Schema(implementation = ApiResponceListUsersDTO.class))
+            ),
+            parameters = {
+                    @Parameter(description = "Значение возраста от (включительно)", name = "ageOne", required = true),
+                    @Parameter(description = "Значение возраста до (включительно)", name="ageTwo", required = true)
+            })
+    public List<Users> getUsersBetweenAge(@RequestParam int ageOne, @RequestParam  int ageTwo ){
+        return usersService.getAgeUserBetween(ageOne, ageTwo);
+        }
+
+    @GetMapping("/findByNameAndSurname")
+    @Operation(
+            summary = "Получение пользователе по возрасту и имени",
+            responses = @ApiResponse(
+                    responseCode = "200", content = @Content(schema = @Schema(implementation = ApiResponceListUsersDTO.class))
+            ),
+            parameters = {
+                    @Parameter(description = "Имя пользователя", name = "name", required = true),
+                    @Parameter(description = "Фамилия пользователя", name="surname", required = true)
+            }
+    )
+    public List<Users> findUsersByNameAndSurname(@RequestParam String name, @RequestParam String surname){
+        return usersService.findByNameAndSurname(name, surname);
+    }
+    }
+
--- src/main/java/com/webapp/springBoot/service/UsersService.java
+++ src/main/java/com/webapp/springBoot/service/UsersService.java
@@ -1,13 +1,18 @@
 package com.webapp.springBoot.service;
 
 
+import com.webapp.springBoot.DTO.Person.APiResponceUserDTO;
 import com.webapp.springBoot.entity.Users;
+import com.webapp.springBoot.exception.ValidationErrorWithMethod;
 import com.webapp.springBoot.repository.UserRepository;
 import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @Service
@@ -15,27 +20,52 @@ public class UsersService {
     @Autowired
     private UserRepository userRepository;
 
-    public void saveUser(Users users){
+    public void saveUser(@Valid APiResponceUserDTO aPiResponceUserDTO, BindingResult result) throws ValidationErrorWithMethod {
+        if(result.hasErrors()){
+            throw  new ValidationErrorWithMethod(result.getAllErrors());
+        }
+        Users users = new Users(
+                aPiResponceUserDTO.getName(),
+                aPiResponceUserDTO.getSurname(),
+                aPiResponceUserDTO.getAge()
+        );
         userRepository.save(users);
     }
 
     public List<Users> getUserByName(String name){
-        return userRepository.findByName(name);
+        List<Users> users = userRepository.findByName(name);
+        if (users.isEmpty()){
+            throw new NoSuchElementException("Имя пользователя не найдено");
+        }
+        return users;
     }
 
     public List<Users> getAllUser(){
         return userRepository.findAll();
     }
 
+    public List<Users> getAgeUserBetween(int ageOne, int ageTwo){
+        return userRepository.getUsersByAgeBetween(ageOne, ageTwo);
+    }
+
     @Transactional
-    public Users deleteUserByID(Long id) throws Exception {
+    public void deleteUserByID(Long id) throws NoSuchElementException {
         Optional<Users> users = userRepository.findById(id);
         if (users.isEmpty()){
-            throw new Exception("Пользователь не найден");
+            throw new NoSuchElementException("ID пользователя не найден");
         }
-        Users user = users.get();
-        userRepository.delete(user);
-        return user;
+        userRepository.delete(users.get());
     }
+
+    public List<Users> findByNameAndSurname(String name, String surname){
+        List<Users> users = userRepository.findByNameAndSurname(name, surname);
+        System.out.println(users);
+        if (users.isEmpty()){
+            throw new NoSuchElementException("Пользователей с таким именем и фамилией нет");
+        }
+        return users;
+    }
+
+
 
 }
